5aa2594afda789f4f4127ef7ed31f55ff1fe938c
Rhonda Glennon <rhondaglennon@gmail.com>
2018-01-28 10:35:32 -0800

    add index file, make progress on getting values from buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,14 @@ $(document).ready(function(){
 
 
 var intervalId;
+// $("input[type='radio']").on("click", function() {
+//   var radioValue = $("input[name='exampleRadios']:checked").val();
+//   console.log(radioValue);
+// });
+
+trivia.startTimer();
+
+});
 
 var trivia = {
     //make this 120 seconds when actually playing game
@@ -20,81 +28,48 @@ var trivia = {
   stopTimer: function() {
     clearInterval(intervalId);
     console.log("time stopped");
+    trivia.testing();
     // end game, tally score, show new page with score
   },
 
   countdown: function() {
     trivia.timeRemaining--;
     $("#timer").text(trivia.timeRemaining);
-    console.log(trivia.timeRemaining);
+    // console.log(trivia.timeRemaining);
     if (trivia.timeRemaining === 0) {
       trivia.stopTimer();
     }
   },
 
+  testing: function () {
+    var radioValue = $("input[name='exampleRadios']:checked").val();
+    console.log(radioValue);
+  },
   // keepScore: function() {
   //   // tally up correct, incorrect, unanswered
   // }
 
+// TODO: make this in a loop
   displayQuestions: function() {
-    $("#question").text(trivia.questionsList.question);
-    $("#customRadio1label").text(trivia.questionsList.incorrect_answers[0]);
-  },
-
-  questionsList:
-    {
-
-    }
-
-  {
-    "category": "Entertainment: Cartoon & Animations",
-    "type": "multiple",
-    "difficulty": "medium",
-    "question": "What is Scooby-Doo&#039;s real name?",
-    "correct_answer": "Scoobert",
-    "incorrect_answers": [
-      "Scooter",
-      "Scrappy",
-      "Scoobert"
-    ]
-}
-
-{
-  "category": "Entertainment: Cartoon & Animations",
-  "type": "multiple",
-  "difficulty": "medium",
-  "question": "Adam West was the mayor of which cartoon town?",
-  "correct_answer": "Quahog",
-  "incorrect_answers": [
-    "Springfield",
-    "South Park",
-    "Langley Falls"
-  ]
-},
-
-{
-  "category": "Entertainment: Cartoon & Animations",
-  "type": "multiple",
-  "difficulty": "medium",
-  "question": "In &quot;The Simpsons&quot;, where did Homer and Marge first meet?",
-  "correct_answer": "At Summer Camp",
-  "incorrect_answers": [
-    "At High School",
-    "At Church",
-    "At 742 Evergreen Terrace"
-  ]
-},
+    var question = $("#question").text(this.simpsonsQuestions.question1.question);
+    var answer1 = $("#exampleRadios1label").text(this.simpsonsQuestions.question1.answers[0]);
+    var answer2 = $("#exampleRadios2label").text(this.simpsonsQuestions.question1.answers[1]);
+    var answer3 = $("#exampleRadios3label").text(this.simpsonsQuestions.question1.answers[2]);
+    var correctAnswer = trivia.simpsonsQuestions.question1.correct;
 
 
-}
-
-trivia.startTimer();
+// var radioValue = $("input[name='exampleRadios']:checked").val();
+    },
+    // tallyAnswers : function() {
+    //   var radioValue = $("input[name='exampleRadios']:checked").val()
+    //   console.log(radioValue);
+    // },
 
 simpsonsQuestions : {
   question1: {
     question: "Who is the mayor of Springfield?",
-    answers: ["Joe Quimby", "Montgomery Burns", "Clancy Wiggum"],
-    correct: "Clancy Wiggum"
+    answers: ["Montgomery Burns", "Joe Quimby", "Clancy Wiggum"],
+    correct: "Joe Quimby"
   },
 
   question2: {
@@ -111,38 +86,37 @@ simpsonsQuestions : {
     question: "When Bart rents a car with a fake license, where does he drive with his friends on spring break?",
     answers: ["Shelbyville", "Itchy and Scratchy Land", "Knoxville"],
     correct: "Knoxville"
+  },
   question5: {
     question: "When the Simpsons move to a farm, what do they plant and sell at a roadside stand?",
     answers: ["carrots and peas", "a cross of tomatoes and tobacco", "gummy bears"],
     correct: "a cross of tomatoes and tobacco"
-  }
+  },
   question6: {
     question: "What character that Homer voices gets added to the Itchy and Scratchy Show?",
     answers: ["Poochie", "Max Power", "Kang"],
     correct: "Poochie"
-  }
+  },
   question7: {
     question: "When the Simpson kids are removed by child protective services, who is their foster home?",
     answers: ["Flanders", "Van Houten", "Smithers"],
     correct: "Flanders"
-  }
+  },
   question8: {
     question: "When Marge starts a business after attending a franchise fair, what is her company?",
     answers: ["Fleet-a-Pita", "Flancrest Enterprises", "Pretzel Wagon"],
     correct: "Pretzel Wagon"
-  }
+  },
   question9: {
-    question: "What dish does Lisa bring to Homer's "BBBQ" backyard barbecue?",
-    answers: "salad", "gazpacho", "lamb chops",
+    question: "What does Lisa make for Homer's BBBQ backyard barbecue?",
+    answers: ["salad", "gazpacho", "lamb chops"],
     correct: "gazpacho"
-  }
+  },
   question10: {
-    question: "What is Seymour Skinner's real name?",
-    answers: "Troy McClure", "Gary Chalmers","Armin Tamzarian",
+    question: "What is Principal Seymour Skinner's real name?",
+    answers: ["Troy McClure", "Gary Chalmers","Armin Tamzarian"],
     correct: "Armin Tamzarian"
   }
   }
 
 }
-
-});
