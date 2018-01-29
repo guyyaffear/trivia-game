3b465ce2e0b99cf23000da18619a4cac7fed3e03
Rhonda Glennon <rhondaglennon@gmail.com>
2018-01-28 16:37:59 -0800

    generate questions divs from jquery object

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,7 +23,7 @@ var trivia = {
   stopTimer: function() {
     clearInterval(intervalId);
     console.log("time stopped");
-    trivia.checkAnswers();
+    // trivia.checkAnswers();
     // end game, tally score, show new page with score
   },
 
@@ -36,88 +36,112 @@ var trivia = {
     }
   },
 
-  checkAnswers: function() {
-
-  var correctAnswer = trivia.simpsonsQuestions.question1.correct;
-  var userAnswer = $("input:checked + label").text();
-
-  console.log(correctAnswer);
-  console.log(userAnswer);
-
-
-if (userAnswer === correctAnswer) {
-     console.log("celebrate");
-  }
-
-  },
+//   checkAnswers: function() {
+//
+//   var correctAnswer = simpsonsQuestions.correct;
+//   var userAnswer = $("input:checked + label").text();
+//
+//   console.log(correctAnswer);
+//   console.log(userAnswer);
+//
+//
+// if (userAnswer === correctAnswer) {
+//      console.log("celebrate");
+//   }
+//
+//   },
   // keepScore: function() {
   //   // tally up correct, incorrect, unanswered
 // },
 
 // TODO: make this in a loop
   displayQuestions: function() {
-    var question = $("#question").text(this.simpsonsQuestions.question1.question);;
-    var answer1 = this.simpsonsQuestions.question1.answers[0];
-    $("#radio1label").text(answer1);
+    // var question = $("#question").text(this.simpsonsQuestions.question1.question);;
+    // var answer1 = this.simpsonsQuestions.question1.answers[0];
+    // $("#radio1label").text(answer1);
+    //
+    // var answer2 = this.simpsonsQuestions.question1.answers[1];
+    // $("#radio2label").text(answer2);
+    // var answer3 = this.simpsonsQuestions.question1.answers[2];
+    // $("#radio3label").text(answer3);
+      var divContainer = $("#questions-box");
+      var answerGroup = $(".form-check");
 
-    var answer2 = this.simpsonsQuestions.question1.answers[1];
-    $("#radio2label").text(answer2);
-    var answer3 = this.simpsonsQuestions.question1.answers[2];
-    $("#radio3label").text(answer3);
+
+    for (var i = 0; i < simpsonsQuestions.length; i++) {
+
+      divContainer.append('<div id="question">' + simpsonsQuestions[i].question + '</div>');
+
+      var answer1 = simpsonsQuestions[i].answers[0];
+      var answer2 = simpsonsQuestions[i].answers[1];
+            var answer3 = simpsonsQuestions[i].answers[2];
+
+      divContainer.append('<div class="form-check"><input class="form-check-input" type="radio" name="radio-answer" id="radio'+i+'"><label class="form-check-label" id="radio'+i+'label" for="radio'+i+'">' + answer1 +
+              '</label></div>');
+      divContainer.append('<div class="form-check"><input class="form-check-input" type="radio" name="radio-answer" id="radio'+i+'"><label class="form-check-label" id="radio'+i+'label" for="radio'+i+'">' + answer2 +
+                      '</label></div>');
+    divContainer.append('<div class="form-check"><input class="form-check-input" type="radio" name="radio-answer" id="radio'+i+'"><label class="form-check-label" id="radio'+i+'label" for="radio'+i+'">' + answer3 +
+                                      '</label></div>');
+
+    }
+
+
     },
 
-simpsonsQuestions : {
-  question1: {
+
+}
+
+var simpsonsQuestions =
+[
+   {
     question: "Who is the mayor of Springfield?",
     answers: ["Montgomery Burns", "Joe Quimby", "Clancy Wiggum"],
     correct: "Joe Quimby"
   },
 
-  question2: {
+  {
     question: "What did Lyle Lanley sell to Brockway, Ogdenville, and North Haverbrook?",
     answers: ["monorail", "nuclear power plant", "mule"],
     correct: "monorail"
   },
-  question3: {
+  {
     question: "What what the name of Bart's elephant?",
     answers: ["Snowball", "Stampy", "Laddie"],
     correct: "Stampy"
   },
-  question4: {
+  {
     question: "When Bart rents a car with a fake license, where does he drive with his friends on spring break?",
     answers: ["Shelbyville", "Itchy and Scratchy Land", "Knoxville"],
     correct: "Knoxville"
   },
-  question5: {
+  {
     question: "When the Simpsons move to a farm, what do they plant and sell at a roadside stand?",
     answers: ["carrots and peas", "a cross of tomatoes and tobacco", "gummy bears"],
     correct: "a cross of tomatoes and tobacco"
   },
-  question6: {
+  {
     question: "What character that Homer voices gets added to the Itchy and Scratchy Show?",
     answers: ["Poochie", "Max Power", "Kang"],
     correct: "Poochie"
   },
-  question7: {
+  {
     question: "When the Simpson kids are removed by child protective services, who is their foster home?",
     answers: ["Flanders", "Van Houten", "Smithers"],
     correct: "Flanders"
   },
-  question8: {
+  {
     question: "When Marge starts a business after attending a franchise fair, what is her company?",
     answers: ["Fleet-a-Pita", "Flancrest Enterprises", "Pretzel Wagon"],
     correct: "Pretzel Wagon"
   },
-  question9: {
+  {
     question: "What does Lisa make for Homer's BBBQ backyard barbecue?",
     answers: ["salad", "gazpacho", "lamb chops"],
     correct: "gazpacho"
   },
-  question10: {
+  {
     question: "What is Principal Seymour Skinner's real name?",
     answers: ["Troy McClure", "Gary Chalmers","Armin Tamzarian"],
     correct: "Armin Tamzarian"
   }
-  }
-
-}
+]
